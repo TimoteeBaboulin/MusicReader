a468fc24214df09ab479a4d27c3a016f600736be
Timotee Baboulin <timotee.baboulin@gmail.com>
2025-11-03 14:29:34 +0100

    Fixed mickael not being allowed near my code

--- WinformCplusplus/OGGReader.h
+++ WinformCplusplus/OGGReader.h
@@ -15,11 +15,12 @@ class OGGReader
 public:
 	static void Init();
 	static void Play(const char* _path);
-private:
-	static bool TryLoadOGG(const char* _path, OGGData* _data);
 	static void Pause();
 	static void Resume();
 	static void End();
+
+private:
+	static bool TryLoadOGG(const char* _path, OGGData* _data);
 	static ALuint m_Buffer;
 	static ALuint m_Source;
 };
